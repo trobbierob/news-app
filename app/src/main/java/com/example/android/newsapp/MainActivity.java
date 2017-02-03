71c63535e9b4aec7db822f77f20a08c073472d6c
trobbierob <troberts@my.hpu.edu>
2017-02-03 18:02:55 -0500

    Add Loader
    Removed AsynsTask to create AsyncTaskLoader. Need to make changes to Loader to resolve the empty list when rotating

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.android.newsapp;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
@@ -28,12 +30,14 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<String>{
 
     private EditText mSearchEditText;
     private TextView mUrlDisplayTextView;
     private TextView mSearchResultsTextView;
     private SearchView mSearchView;
+    private static final int NEWS_SEARCH_LOADER = 1;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private ProgressBar mLoadingIndicator;
     private String webTitle;
@@ -42,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     public URL guardianQueryUrl;
     private ListView listView;
     private ArrayList<HashMap<String, String>> newsList;
+    private static final String SEARCH_QUERY = "searchQuery";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,108 @@ public class MainActivity extends AppCompatActivity {
         newsList = new ArrayList<>();
         mEmptyView = (TextView) findViewById(R.id.empty);
         listView = (ListView) findViewById(R.id.list);
+        getSupportLoaderManager().initLoader(NEWS_SEARCH_LOADER, null, this);
+    }
+
+    private void Activate() {
+
+        String userQuery = mSearchEditText.getText().toString();
+
+        if (userQuery.equals(null) || userQuery.equals("")){
+            mEmptyView.setVisibility(View.VISIBLE);
+            listView.setVisibility(View.INVISIBLE);
+
+        } else {
+            mEmptyView.setVisibility(View.INVISIBLE);
+            listView.setVisibility(View.VISIBLE);
+            guardianQueryUrl = NetworkUtils.buildURL(userQuery);
+        }
+
+        Bundle bundle = new Bundle();
+        bundle.putString(SEARCH_QUERY, guardianQueryUrl.toString());
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<String> searchLoader = loaderManager.getLoader(NEWS_SEARCH_LOADER);
+
+        if (searchLoader == null) {
+            loaderManager.initLoader(NEWS_SEARCH_LOADER, bundle, MainActivity.this);
+
+        } else {
+            loaderManager.restartLoader(NEWS_SEARCH_LOADER, bundle, MainActivity.this);
+        }
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+
+            @Override
+            protected void onStartLoading() {
+                if (args == null) {
+                    return;
+                }
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+                forceLoad();
+            }
+
+            @Override
+            public String loadInBackground() {
+                if (guardianQueryUrl != null) {
+                    try {
+                        jsonString = NetworkUtils.getResponseFromHttpUrl(guardianQueryUrl);
+                        JSONObject jsonNewsRootObject = new JSONObject(jsonString);
+                        Log.v(LOG_TAG, "jsonrootObject is: " + jsonNewsRootObject);
+                        JSONObject responseObject = jsonNewsRootObject.optJSONObject("response");
+                        Log.v(LOG_TAG, "Response Object is: " + responseObject);
+                        JSONArray resultsArray = responseObject.optJSONArray("results");
+                        Log.v(LOG_TAG, "Results Array is: " + resultsArray);
+
+                        for (int i = 0; i < resultsArray.length(); i++) {
+                            JSONObject jsonObject = resultsArray.getJSONObject(i);
+                            Log.v(LOG_TAG, "jsonObject is: " + jsonObject);
+
+                            if (jsonObject.has("webTitle")){
+                                webTitle = jsonObject.optString("webTitle");
+                            } else {
+                                webTitle = getString(R.string.title_missing);
+                            }
+
+                            Log.v(LOG_TAG, "webTitle is: " + webTitle);
+
+                            HashMap<String, String> newsArticle = new HashMap<>();
+
+                            newsArticle.put(getString(R.string.webTitle), webTitle);
+                            newsList.add(newsArticle);
+                        }
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "IOException at " + e);
+                    } catch (JSONException e) {
+                        Log.e(LOG_TAG, "JSONException at " + e);
+                    }
+
+                } else {
+                    Log.e(LOG_TAG, "JSON Server Error");
+                }
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        if (newsList != null) {
+            Log.v(LOG_TAG, "newsList is: " + newsList);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, newsList,
+                    R.layout.list_item, new String[]{getString(R.string.webTitle)},
+                    new int[]{R.id.webTitle});
+            listView.setAdapter(adapter);
+        } else {
+            listView.setEmptyView(findViewById(R.id.empty));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
 
     }
 
@@ -71,10 +178,9 @@ public class MainActivity extends AppCompatActivity {
                     MainActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
             if (activeNetwork != null) { // connected to the internet
-
                 DisplayURLText();
                 newsList.clear();
-                new GuardianQueryTask().execute();
+                Activate();
             } else { // not connected to the internet
                 Toast.makeText(getBaseContext(), "Check Connection",
                         Toast.LENGTH_SHORT).show();
@@ -90,79 +196,5 @@ public class MainActivity extends AppCompatActivity {
         String githubQuery = mSearchEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildURL(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
-    }
-
-    public class GuardianQueryTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-            String userQuery = mSearchEditText.getText().toString();
-
-            if (userQuery.equals(null) || userQuery.equals("")){
-                mEmptyView.setVisibility(View.VISIBLE);
-                listView.setVisibility(View.INVISIBLE);
-
-            } else {
-                mEmptyView.setVisibility(View.INVISIBLE);
-                listView.setVisibility(View.VISIBLE);
-                guardianQueryUrl = NetworkUtils.buildURL(userQuery);
-            }
-        }
-
-        @Override
-        protected Void doInBackground(Void... urls) {
-            if (guardianQueryUrl != null) {
-                try {
-                    jsonString = NetworkUtils.getResponseFromHttpUrl(guardianQueryUrl);
-                    JSONObject jsonNewsRootObject = new JSONObject(jsonString);
-                    Log.v(LOG_TAG, "jsonrootObject is: " + jsonNewsRootObject);
-                    JSONObject responseObject = jsonNewsRootObject.optJSONObject("response");
-                    Log.v(LOG_TAG, "Response Object is: " + responseObject);
-                    JSONArray resultsArray = responseObject.optJSONArray("results");
-                    Log.v(LOG_TAG, "Results Array is: " + resultsArray);
-
-                    for (int i = 0; i < resultsArray.length(); i++) {
-                        JSONObject jsonObject = resultsArray.getJSONObject(i);
-                        Log.v(LOG_TAG, "jsonObject is: " + jsonObject);
-
-                        if (jsonObject.has("webTitle")){
-                            webTitle = jsonObject.optString("webTitle");
-                        } else {
-                            webTitle = getString(R.string.title_missing);
-                        }
-
-                        Log.v(LOG_TAG, "webTitle is: " + webTitle);
-
-                        HashMap<String, String> newsArticle = new HashMap<>();
-
-                        newsArticle.put(getString(R.string.webTitle), webTitle);
-                        newsList.add(newsArticle);
-                    }
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "IOException at " + e);
-                } catch (JSONException e) {
-                    Log.e(LOG_TAG, "JSONException at " + e);
-                }
-
-            } else {
-                Log.e(LOG_TAG, "JSON Server Error");
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (newsList != null) {
-                Log.v(LOG_TAG, "newsList is: " + newsList);
-                ListAdapter adapter = new SimpleAdapter(MainActivity.this, newsList,
-                        R.layout.list_item, new String[]{getString(R.string.webTitle)},
-                        new int[]{R.id.webTitle});
-                listView.setAdapter(adapter);
-            } else {
-                listView.setEmptyView(findViewById(R.id.empty));
-            }
-        }
     }
 }
